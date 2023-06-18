77502508d737ef8702c7be7a76e3ebef43d32cb3
WantClue <86001033+WantClue@users.noreply.github.com>
2023-06-18 23:12:51 +0200

    regional plans

--- ThingsIX/setup.sh
+++ ThingsIX/setup.sh
@@ -11,7 +11,13 @@ NC='\033[0m'
 
 #paths
 dir_path="/etc/thingsix-forwarder"
-file_path="/etc/global_conf.json"
+file_path_eu="/etc/global_conf.json.sx1250.EU868.template"
+file_path_au="/etc/global_conf.json.sx1250.AU915.template"
+file_path_ru="/etc/global_conf.json.sx1250.RU864.template"
+file_path_kr="/etc/global_conf.json.sx1250.KR920.template"
+file_path_in="/etc/global_conf.json.sx1250.IN865.template"
+file_path_as="/etc/global_conf.json.sx1250.AS923_1.template"
+file_path_us="/etc/global_conf.json.sx1250.US915.template"
 local_id="/etc/thingsix-forwarder/unknown_gateways.yaml"
 
 
@@ -41,11 +47,99 @@ function install() {
         mkdir -p /etc/thingsix-forwarder
     fi
 
-    if whiptail --yesno "Would you like to proceed and change the global conf?" 8 60; then
-        sed -i 's/"serv_port_up": 1680,/"serv_port_up": 1688,/g' $file_path
-        sed -i 's/"serv_port_down": 1680,/"serv_port_down": 1688,/g' $file_path
-    else 
-        exit
+    OPTION=$(whiptail --title "Choose your Region" --menu "Choose the Region you deploy our Gateway" 20 60 10 \
+    "1" "EU 868" \
+    "2" "US 915" \
+    "3" "KR 920" \
+    "4" "AS 923" \
+    "5" "AU 915" \
+    "6" "IN 865" \
+    "7" "RU 864" 3>&1 1>&2 2>&3)
+
+    exitstatus=$?
+    if [ $exitstatus = 0 ]; then
+        echo "Your chosen option: $OPTION"
+        # Based on the chosen option, you change different regional plans
+        case $OPTION in
+        1)
+            echo "EU868 chosen"
+            if whiptail --yesno "Would you like to proceed and change the global conf?" 8 60; then
+                sed -i 's/"serv_port_up": 1680,/"serv_port_up": 1688,/g' $file_path_eu
+                sed -i 's/"serv_port_down": 1680,/"serv_port_down": 1688,/g' $file_path_eu
+            else 
+                exit
+            fi
+            # Execute your command for Option 1 here
+        ;;
+        2)
+            echo "Option 2 chosen"
+            echo "EU868 chosen"
+            if whiptail --yesno "Would you like to proceed and change the global conf?" 8 60; then
+                sed -i 's/"serv_port_up": 1680,/"serv_port_up": 1688,/g' $file_path_us
+                sed -i 's/"serv_port_down": 1680,/"serv_port_down": 1688,/g' $file_path_us
+            else 
+                exit
+            fi
+            # Execute your command for Option 2 here
+        ;;
+        3)
+            echo "Option 3 chosen"
+            echo "EU868 chosen"
+            if whiptail --yesno "Would you like to proceed and change the global conf?" 8 60; then
+                sed -i 's/"serv_port_up": 1680,/"serv_port_up": 1688,/g' $file_path_kr
+                sed -i 's/"serv_port_down": 1680,/"serv_port_down": 1688,/g' $file_path_kr
+            else 
+                exit
+            fi
+            # Execute your command for Option 3 here
+        ;;
+        4)
+            echo "Option 4 chosen"
+            echo "EU868 chosen"
+            if whiptail --yesno "Would you like to proceed and change the global conf?" 8 60; then
+                sed -i 's/"serv_port_up": 1680,/"serv_port_up": 1688,/g' $file_path_as
+                sed -i 's/"serv_port_down": 1680,/"serv_port_down": 1688,/g' $file_path_as
+            else 
+                exit
+            fi
+            # Execute your command for Option 4 here
+        ;;
+        5)
+            echo "Option 5 chosen"
+            echo "EU868 chosen"
+            if whiptail --yesno "Would you like to proceed and change the global conf?" 8 60; then
+                sed -i 's/"serv_port_up": 1680,/"serv_port_up": 1688,/g' $file_path_au
+                sed -i 's/"serv_port_down": 1680,/"serv_port_down": 1688,/g' $file_path_au
+            else 
+                exit
+            fi
+            # Execute your command for Option 5 here
+        ;;
+        6)
+            echo "Option 6 chosen"
+            echo "EU868 chosen"
+            if whiptail --yesno "Would you like to proceed and change the global conf?" 8 60; then
+                sed -i 's/"serv_port_up": 1680,/"serv_port_up": 1688,/g' $file_path_in
+                sed -i 's/"serv_port_down": 1680,/"serv_port_down": 1688,/g' $file_path_in
+            else 
+                exit
+            fi
+            # Execute your command for Option 6 here
+        ;;
+        7)
+            echo "Option 7 chosen"
+            echo "EU868 chosen"
+            if whiptail --yesno "Would you like to proceed and change the global conf?" 8 60; then
+                sed -i 's/"serv_port_up": 1680,/"serv_port_up": 1688,/g' $file_path_ru
+                sed -i 's/"serv_port_down": 1680,/"serv_port_down": 1688,/g' $file_path_ru
+            else 
+                exit
+            fi
+            # Execute your command for Option 7 here
+        ;;
+        esac
+    else
+        echo "You chose Cancel."
     fi
 
     if [[ $(docker ps -f "name=gwmp-mux" --format "{{.Names}}") == "gwmp-mux" ]]; then
